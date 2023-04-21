1a68a32313925323884745e7cec385d424d287ad
Jhernandezm1246 <jhmbao@gmail.com>
2023-04-20 20:33:10 -0600

    Agregar Formulario

--- ProjectoFinalJoaoHernandez/src/Formulario.jsx
+++ ProjectoFinalJoaoHernandez/src/Formulario.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import './Formulario.css'
 const Formulario = () => {
+    const[pacientes,setPacientes]=useState([])
+    const[mascota,setMascota]=useState("")
+    const[duenno,setDuenno]=useState("")
+
+    function agregarPacientes(){
+        const informacion={
+            mascota,
+            duenno
+        }
+        setPacientes([...pacientes,mascota])
+        setMascota("")
+        setDuenno("")
+    }
+
+
   return (
-    <div>Formulario</div>
+    <div>
+        <form className='inputForm' action="">
+            <label htmlFor="">Nombre Mascota </label>
+            <input onChange={(e)=>{setMascota(e.target.value)}} value={mascota} type="text" />
+            <label htmlFor="">Nombre Dueño </label>
+            <input onChange={(e)=>{setDuenno(e.target.value)}} value={duenno} type="text" />
+            <label htmlFor="">Fecha </label>
+            <input type="date" />
+            <label htmlFor="">Hora </label>
+            <input type="hour" />
+            <label htmlFor="">Sintomas</label>
+            <textarea type="text" />
+            <button onClick={agregarPacientes} type='button'>Agregar</button>
+
+        </form>
+    </div>
   )
 }
 
